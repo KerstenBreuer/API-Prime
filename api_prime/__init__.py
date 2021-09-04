53eb81f85a934cca380fe42408305d85d085ee95
KerstenBreuer <kersten-breuer@outlook.com>
2021-09-04 17:53:14 +0000

    adapt describtion

--- api_prime/__init__.py
+++ api_prime/__init__.py
@@ -1,3 +1,4 @@
-"""An async framework for API-first web app design based on OpenAPI and Starlette"""
+"""An ASGI framework for documentation-driven API design (API-first)
+based on OpenAPI 3.0+ and Starlette"""
 
 __version__ = "0.1.0"
